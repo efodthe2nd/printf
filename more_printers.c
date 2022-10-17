37fe58a2aded8fe83e2e72a63b9ff87c7e149a98
felix <felix.okoronkwo.1@gmail.com>
2022-10-17 22:51:29 +0000

    initial commit

--- more_printers.c
+++ more_printers.c
@@ -2,47 +2,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * _print_int_binary - Prints a int converted to binary
- * @args: A list of variadic arguments
- *
- * Return: the number of printed digites
- */
+int base_converter(int x, int base)
+{
+	int y, b, count = 0;
+	int alloc = 1;
+	char *str;
+
+	str = malloc(alloc);
+	if (x == 0)
+	{
+		_write(0 + '0');
+		return (1);
+	}
+
+	if (x < 0)
+	{
+		_write('-');
+		x = x * -1;
+		count++;
+	}
+
+	while (x > 0)
+	{
+		y = x % base;
+		str[count] = y + '0';
+		str = realloc(str, ++alloc);
+		count++;
+		x = (x / base);
+	}
+	str[count] = '\0';
+
+	b = count;
+
+	while (b >= 0)
+	{
+		_write(str[b]);
+		b--;
+	}
+
+	free(str);
+
+	return (count);
+}
 
 int _print_int_binary(va_list args)
 {
-	unsigned int x = 0;
-	int b = 0, new = 0;
+	int count;
 
-	new = va_arg(args, int);
-	x = new;
-	if (new < 0)
-	{
-		_write('1');
-		new = new * -1;
-		x = new;
-		b += 1;
-	}
-	while (x > 0)
-	{
-		x = x / 2;
-		b++;
-	}
-	_recursion_int_binary(new);
-	return (b);
-}
-
-/**
- * _recursion_int_binary - Prints a binary
- * @a: integer to print
- *
- */
-void _recursion_int_binary(int a)
-{
-	unsigned int t;
-
-	t = a;
-	if (t / 2)
-		_recursion_int_binary(t / 2);
-	_write(t % 2 + '0');
+	count = base_converter(va_arg(args, int), 2);
+	return (count);
 }
