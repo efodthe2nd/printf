7017944a7bac15c8eeca78f76055becd8b9dbd9d
efodthe2nd <davidefod@gmail.com>
2022-10-18 19:45:16 +0000

    another fix

--- _write.c
+++ _write.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 
 /**
  * * _write - writes the character c to stdout
@@ -10,4 +11,48 @@
 int _write(char c)
 {
 	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for(i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
+
+
+/**
+ * _putchar - prints a number in hexadecimal
+ * @c: va_list arguments from _printf
+ * @f: pointer to the struct flags
+ *
+ * Description: the function calls convert
+ *
+ * Return: the number of char printed
+ */
+
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
---
+++ print_nums.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+
+/**
+ * print_int - prints a number in hexadecimal
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags
+ *
+ * Description: the function calls convert
+ *
+ * Return: the number of char printed
+ */
+
+int print_int(va_list l, flags_t *f)
+{
+	int n = va_arg(l, int);
+	int res = count_digit(n);
+
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		res += _putchar(' ');
+	if (f->plus == 1 && n >= 0)
+		res += _putchar('+');
+	if (n <= 0)
+		res++;
+	print_number(n);
+	return (res);
+}
+
+
+/**
+ * print_unsigned - prints a number in hexadecimal
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags
+ *
+ * Description: the function calls convert
+ *
+ * Return: the number of char printed
+ */
+
+int print_unsigned(va_list l, flags_t *f)
+{
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
+
+	(void)f;
+	return (_puts(str));
+}
+
+
+/**
+ * print_number - prints a number in hexadecimal
+ * @l: va_list arguments from _printf
+ * @n: pointer to the struct flags
+ *
+ * 
+ * Return: the number of char printed
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
+}
+
+
+/**
+ * count_digit - prints a number in hexadecimal
+ * @i: integer to evaluate
+ *
+ * 
+ * Return: the number of char printed
+ */
+
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+
+	return (d);
+}
